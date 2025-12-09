dfd82fb5fa18386d0cf2f6307c39394983ee7b25
kenzieliu <kqliuchina@163.com>
2025-12-09 23:17:45 +0800

    开启基于注解的AOP功能

--- src/main/java/com/kq/zhipicturebackend/ZhiPictureBackendApplication.java
+++ src/main/java/com/kq/zhipicturebackend/ZhiPictureBackendApplication.java
@@ -3,10 +3,11 @@ package com.kq.zhipicturebackend;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @MapperScan("com.kq.zhipicturebackend.mapper")
-//@EnableAspectJAutoProxy(exposeProxy = true)
+@EnableAspectJAutoProxy(exposeProxy = true)
 public class ZhiPictureBackendApplication {
 
     public static void main(String[] args) {
