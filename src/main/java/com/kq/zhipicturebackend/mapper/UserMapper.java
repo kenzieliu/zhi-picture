ac4fac5ee9f55e9caeabf18ce4e1648c38380cda
kenzieliu <kqliuchina@163.com>
2025-12-11 20:30:08 +0800

    开发用户注册接口

---
+++ src/main/java/com/kq/zhipicturebackend/mapper/UserMapper.java
@@ -0,0 +1,19 @@
+package com.kq.zhipicturebackend.mapper;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.kq.zhipicturebackend.model.entity.User;
+
+/**
+* @author kenzieliu
+* @description 针对表【user(用户)】的数据库操作Mapper
+* @createDate 2024-12-09 20:03:03
+* @Entity com.kq.zhipicturebackend.model.entity.User
+*/
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
