110845da413567543e2018ce50bf1aef232fcb2d
kenzieliu <kqliuchina@163.com>
2025-12-21 22:57:28 +0800

    图片模块优化 1. 图片查询优化 - 多级缓存 2. 图片上传优化 - 图片压缩 3. 图片加载优化 - 缩略图 4. 图片清理优化 - 清理文件 5. 使用Redis管理Session，维护登录态

--- src/main/java/com/kq/zhipicturebackend/service/PictureService.java
+++ src/main/java/com/kq/zhipicturebackend/service/PictureService.java
@@ -92,4 +92,11 @@ public interface PictureService extends IService<Picture> {
     Integer uploadPictureByBatch(PictureUploadByBatchRequest pictureUploadByBatchRequest,
                          User loginUser);
 
+    /**
+     * 清理图片文件
+     *
+     * @param oldPicture
+     */
+    void clearPictureFile(Picture oldPicture);
+
 }
--- src/main/java/com/kq/zhipicturebackend/service/impl/PictureServiceImpl.java
+++ src/main/java/com/kq/zhipicturebackend/service/impl/PictureServiceImpl.java
@@ -10,6 +10,7 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.kq.zhipicturebackend.exception.BusinessException;
 import com.kq.zhipicturebackend.exception.ErrorCode;
 import com.kq.zhipicturebackend.exception.ThrowUtils;
+import com.kq.zhipicturebackend.manager.CosManager;
 import com.kq.zhipicturebackend.manager.FileManager;
 import com.kq.zhipicturebackend.manager.upload.FilePictureUpload;
 import com.kq.zhipicturebackend.manager.upload.PictureUploadTemplate;
@@ -32,6 +33,8 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -64,6 +67,9 @@ public class PictureServiceImpl extends ServiceImpl<PictureMapper, Picture>
 
     @Resource
     private UrlPictureUpload urlPictureUpload;
+
+    @Autowired
+    private CosManager cosManager;
 
     @Override
     public void validPicture(Picture picture) {
@@ -113,6 +119,7 @@ public class PictureServiceImpl extends ServiceImpl<PictureMapper, Picture>
         // 构造要入库的图片信息
         Picture picture = new Picture();
         picture.setUrl(uploadPictureResult.getUrl());
+        picture.setThumbnailUrl(uploadPictureResult.getThumbnailUrl());
         // 支持外层传递图片名称
         String picName = uploadPictureResult.getPicName();
         if (pictureUploadRequest != null && StrUtil.isNotBlank(pictureUploadRequest.getPicName())) {
@@ -350,6 +357,27 @@ public class PictureServiceImpl extends ServiceImpl<PictureMapper, Picture>
             }
         }
         return uploadCount;
+    }
+
+    @Async
+    @Override
+    public void clearPictureFile(Picture oldPicture) {
+        // 判断改图片是否被多条记录使用
+        String pictureUrl = oldPicture.getUrl();
+        long count = this.lambdaQuery()
+                .eq(Picture::getUrl, pictureUrl)
+                .count();
+        // 有不止一条记录用到了该图片，不清理
+        if (count > 1) {
+            return;
+        }
+        // 删除图片
+        cosManager.deleteObject(pictureUrl);
+        // 删除缩略图
+        String thumbnailUrl = oldPicture.getThumbnailUrl();
+        if (StrUtil.isNotBlank(thumbnailUrl)) {
+            cosManager.deleteObject(thumbnailUrl);
+        }
     }
 }
 
