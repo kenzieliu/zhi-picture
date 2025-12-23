721ac1267331137b2f8dc855dc2bbb74b86f419a
kenzieliu <kqliuchina@163.com>
2025-12-23 15:54:32 +0800

    空间模块开发 1. 管理空间（增删改查） 2. 用户创建私有空间 3. 私有空间权限控制 4. 空间级别和限额控制

---
+++ src/main/java/com/kq/zhipicturebackend/mapper/SpaceMapper.java
@@ -0,0 +1,18 @@
+package com.kq.zhipicturebackend.mapper;
+
+import com.kq.zhipicturebackend.model.entity.Space;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author kenzieliu
+* @description 针对表【space(空间)】的数据库操作Mapper
+* @createDate 2025-12-18 19:53:34
+* @Entity com.kq.zhipicturebackend.model.entity.Space
+*/
+public interface SpaceMapper extends BaseMapper<Space> {
+
+}
+
+
+
+
--- src/main/java/com/kq/zhipicturebackend/service/PictureService.java
+++ src/main/java/com/kq/zhipicturebackend/service/PictureService.java
@@ -3,10 +3,7 @@ package com.kq.zhipicturebackend.service;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.IService;
-import com.kq.zhipicturebackend.model.dto.picture.PictureQueryRequest;
-import com.kq.zhipicturebackend.model.dto.picture.PictureReviewRequest;
-import com.kq.zhipicturebackend.model.dto.picture.PictureUploadByBatchRequest;
-import com.kq.zhipicturebackend.model.dto.picture.PictureUploadRequest;
+import com.kq.zhipicturebackend.model.dto.picture.*;
 import com.kq.zhipicturebackend.model.entity.Picture;
 import com.kq.zhipicturebackend.model.entity.User;
 import com.kq.zhipicturebackend.model.vo.PictureVO;
@@ -98,5 +95,29 @@ public interface PictureService extends IService<Picture> {
      * @param oldPicture
      */
     void clearPictureFile(Picture oldPicture);
+
+    /**
+     * 删除图片
+     *
+     * @param pictureId
+     * @param loginUser
+     */
+    void deletePicture(long pictureId, User loginUser);
+
+    /**
+     * 编辑图片
+     *
+     * @param pictureEditRequest
+     * @param loginUser
+     */
+    void editPicture(PictureEditRequest pictureEditRequest, User loginUser);
+
+    /**
+     * 校验空间图片的权限
+     *
+     * @param loginUser
+     * @param picture
+     */
+    void checkPictureAuth(User loginUser, Picture picture);
 
 }
